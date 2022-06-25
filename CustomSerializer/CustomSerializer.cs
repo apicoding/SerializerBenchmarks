7c80da3065d71bec7f46dfd738da8f9ceaa741e2
apicoding <apicodingprj@gmail.com>
2022-06-25 21:54:26 +0200

    Trying unsafe code

--- CustomSerializer/CustomSerializer.cs
+++ CustomSerializer/CustomSerializer.cs
@@ -1,6 +1,5 @@
 ﻿
 using System.Text;
-using Mono.Cecil.Cil;
 
 namespace SerializerBenchmarks.CustomSerializer
 {
@@ -15,28 +14,6 @@ namespace SerializerBenchmarks.CustomSerializer
                 sb.Append(it.Key);
                 sb.Append(':');
                 sb.Append(it.Value);
-                sb.Append('|');
-            }
-
-            return sb.ToString();
-        }
-
-        public static string Serialize2(MessageCust messageCust)
-        {
-            var sb = new StringBuilder();
-
-            var body = messageCust.Body;
-
-            using var keysEnum = body.Keys.GetEnumerator();
-            using var valuesEnum = body.Values.GetEnumerator();
-
-            for (var i = 0 ; i < body.Count ; i++)
-            {
-                keysEnum.MoveNext();
-                sb.Append(keysEnum.Current);
-                sb.Append(':');
-                valuesEnum.MoveNext();
-                sb.Append(valuesEnum.Current);
                 sb.Append('|');
             }
 
@@ -60,5 +37,10 @@ namespace SerializerBenchmarks.CustomSerializer
 
             return messageCust;
         }
+
+
+        //private static unsafe string Serialize2(MessageCust messageCust)
+        //{
+        //}
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using SerializerBenchmarks.CustomSerializer;
 
 //MessageCust message = new()
 //{
-//   Body = new Dictionary<string, string>()
+//    Body = new Dictionary<string, string>()
 //   {
 //        { "key1", "value1" },
 //        { "key2", "value2" },
@@ -15,14 +15,26 @@ using SerializerBenchmarks.CustomSerializer;
 //        { "key7", "value1" },
 //        { "key8", "value2" },
 //        { "key9", "value1" },
-//        { "key10", "value2" }
+//        { "key10", "value2" },
+//        { "key11", "value2" }
 //   }
 //};
+
+//var size = GetSize(message.Body);
+
+//Console.WriteLine("DONE");
+
+
+//unsafe int GetSize(object obj)
+//{
+//    RuntimeTypeHandle th = obj.GetType().TypeHandle;
+//    return *(*(int**)&th + 1);
+//}
 
 //var ser = CustomSerializer.Serialize2(message);
 
 //var newMessage = CustomSerializer.Deserialize(ser);
 
-//Console.WriteLine("DONE");
+
 
 var summary = BenchmarkRunner.Run<FlatBuffers_Vs_Protobuf_Vs_Utf8Json>();
